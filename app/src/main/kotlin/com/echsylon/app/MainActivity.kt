2da7a757f623a4b89f54512956f0ef9860be2496
László Urszuly <laszlo@echsylon.com>
2022-03-21 12:32:46 +0100

    Switch to view binding in MainActivity

--- app/src/main/kotlin/com/echsylon/app/MainActivity.kt
+++ app/src/main/kotlin/com/echsylon/app/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.echsylon.app
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.echsylon.app.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: MainActivityBinding by lazy { MainActivityBinding.inflate(LayoutInflater.from(this)) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
